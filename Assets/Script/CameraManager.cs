5f536ecd60a01b6b7350a6a2148dfb344f332a76
Trigve <trigves@yahoo.com>
2013-03-13 18:42:48 +0100

    camera: Camera is now positioned "inside" terrain and can move also only inside

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class CameraManager : MonoBehaviour
 {
+#region Enums
+	private enum Direction
+	{
+		LEFT,
+		RIGHT,
+		TOP,
+		BOTTOM,
+	}
+#endregion
 	// Attributes
 	
 	//! Offset.
@@ -13,19 +22,97 @@ public class CameraManager : MonoBehaviour
 	public float WorldWidthRatio = 70F;
 	//! Max Y.
 	public float MaxY = 20F;
-	
+	//! Amount to shift camera.
+	public int amount = 1;
 	//! Camera angle.
 	private const float Angle = 30F;
+	//! Initial window size.
+	private Rect initialWindowSize = new Rect();
 	
 	// Use this for initialization
 	void Awake ()
 	{
+		RecalculateCamera();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Recalculate camera on windo size
+		if (initialWindowSize.width != camera.pixelWidth || initialWindowSize.height != camera.pixelHeight)
+			RecalculateCamera();
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			if(CheckDirection(Direction.LEFT))
+				transform.Translate(0, 0, -amount, Space.World);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			if(CheckDirection(Direction.RIGHT))
+				transform.Translate(0, 0, amount, Space.World);
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			if(CheckDirection(Direction.BOTTOM))
+				transform.Translate(1, 0, 0, Space.World);
+		}
+		else if (Input.GetKey(KeyCode.UpArrow))
+		{
+			if(CheckDirection(Direction.TOP))
+				transform.Translate(-1, 0, 0, Space.World);
+		}
+	}
+
+	private bool CheckDirection(Direction Dir)
+	{
+		bool ret = false;
+		Vector3 point_to_check;
+		if (Dir == Direction.LEFT || Dir == Direction.BOTTOM)
+			point_to_check = new Vector3(0, 0, 0);
+		else
+			point_to_check = new Vector3(1, 1, 0);
+		// Find if we are beyond the terrain
+		Ray ray = Camera.main.ViewportPointToRay(point_to_check);
+		float point;
+		new Plane(Vector3.up, 0).Raycast(ray, out point);
+		Vector3 point_on_plane = ray.GetPoint(point);
+		var terrain_manager = GameObject.Find("Map").GetComponent<TerrainManager>();
+		ja2.Map map = terrain_manager.map;
+		ja2.TerrainTile last_tile = map.GetTile(map.width - 1, map.height - 1);
+		Vector3 last_tile_pos_1 = terrain_manager.GetPosition(last_tile, 1);
+		Vector3 last_tile_pos_2 = terrain_manager.GetPosition(last_tile, 2);
+
+		// Check position
+		switch(Dir)
+		{
+			case Direction.LEFT:
+				ret = point_on_plane.z - amount > ja2.TerrainPartition.TILE_WIDTH;
+				break;
+			case Direction.TOP:
+				ret = point_on_plane.x - amount > ja2.TerrainPartition.TILE_HEIGHT;
+				break;
+			case Direction.BOTTOM:
+				ret = point_on_plane.x + amount < last_tile_pos_2.x;
+				break;
+			case Direction.RIGHT:
+				ret = point_on_plane.z + amount < last_tile_pos_2.z;
+				break;
+		}
+		
+		return ret;
+	}
+
+	private void RecalculateCamera()
+	{
+		// Update size
+		initialWindowSize = camera.pixelRect;
 		// Set orientation
 		transform.rotation = Quaternion.AngleAxis(90, Vector3.down) * Quaternion.AngleAxis(Angle, Vector3.right);
 		
 		float world_width = Screen.width / WorldWidthRatio;
-		float world_height = Screen.height / WorldWidthRatio;
-		
+		float world_height = Screen.height / WorldHeightRatio;
+
 		// Compute length of line of sight AFTER (below) the base plane
 		float qx = (world_height * Mathf.Cos(Mathf.Deg2Rad * Angle) + 2 * MaxY)/ (2 * Mathf.Sin(Mathf.Deg2Rad * Angle));
 		// Compute the whole line of sight without offset
@@ -38,18 +125,13 @@ public class CameraManager : MonoBehaviour
 		camera.farClipPlane = Offset + xw;
 		// Set the dimensions with wide aspect ration
 		camera.orthographicSize = world_width / 2;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (Input.GetKey(KeyCode.LeftArrow))
-			transform.Translate(0, 0, -1, Space.World);
-		else if (Input.GetKey(KeyCode.RightArrow))
-			transform.Translate(0, 0, 1, Space.World);
-		else if (Input.GetKey(KeyCode.DownArrow))
-			transform.Translate(1, 0, 0, Space.World);
-		else if (Input.GetKey(KeyCode.UpArrow))
-			transform.Translate(-1, 0, 0, Space.World);
+		// Find if we are beyond the terrain
+		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0, 0, 0));
+		float point;
+		new Plane(Vector3.up, 0).Raycast(ray, out point);
+		Vector3 point_on_plane = ray.GetPoint(point);
+		// Normalize position
+		transform.Translate(0, 0, -point_on_plane.z + ja2.TerrainPartition.TILE_WIDTH, Space.World);
+		transform.Translate(point_on_plane.x + ja2.TerrainPartition.TILE_HEIGHT, 0, 0, Space.World);
 	}
 }
