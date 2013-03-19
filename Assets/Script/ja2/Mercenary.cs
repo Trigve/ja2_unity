009fd95dd645a215d0685a7d8c0873d62af509a4
Trigve <trigves@yahoo.com>
2013-03-19 22:34:28 +0100

    Initial Mercenary class

---
+++ Assets/Script/ja2/Mercenary.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+namespace ja2
+{
+	public class Mercenary
+	{
+#region Enums
+		public enum LookDirection
+		{
+			EAST = 0,
+			SOUTHEAST = 1,
+			SOUTH = 2,
+			SOUTHWEST = 3,
+			WEST = 4,
+			NORTHWEST = 5,
+			NORTH = 6,
+			NORTHEAST = 7,
+		}
+#endregion
+
+#region Attributes
+		//! Name/Description.
+		public string name;
+		//! Terrain which unit occupies, actual position.
+		public TerrainTile tile;
+		//! Look direction.
+		public LookDirection lookDirection { get; set; }
+#endregion
+
+#region Construction
+		public Mercenary()
+		{
+			// Default direction
+			lookDirection = LookDirection.SOUTHEAST;
+		}
+
+		public Mercenary(TerrainTile Tile)
+			: this()
+		{
+			tile = Tile;
+		}
+#endregion
+	}
+}
