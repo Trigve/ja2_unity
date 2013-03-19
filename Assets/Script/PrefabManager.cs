b6478d12709a7236176f78859f7d544eca7946d8
Trigve <trigves@yahoo.com>
2013-03-19 22:35:41 +0100

    script: Prefab manager class

---
+++ Assets/Script/PrefabManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class PrefabManager
+{
+	//! Instantiate prefab.
+	static public GameObject Create(string Name)
+	{
+		var prefab_class = Resources.Load("Prefabs/" + Name, typeof(GameObject));
+		GameObject prefab = (GameObject)GameObject.Instantiate(prefab_class);
+		prefab.name = prefab_class.name;
+
+		return prefab;
+	}
+}
