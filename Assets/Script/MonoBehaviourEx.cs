8131a7d37fa397f1075dacc942197142a2908e4b
Trigve <trigves@yahoo.com>
2013-03-19 17:33:23 +0100

    script: Base class for MonoBehaviours

---
+++ Assets/Script/MonoBehaviourEx.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+//! Base class for all mono behaviours.
+public abstract class MonoBehaviourEx : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
