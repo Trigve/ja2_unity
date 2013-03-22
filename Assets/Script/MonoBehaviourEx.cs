87fa9cc6be517f5af3321827646882003cb0a7ea
Trigve <trigves@yahoo.com>
2013-03-22 20:44:37 +0100

    script: MonoBehaviourEx remove unused methods for now

--- Assets/Script/MonoBehaviourEx.cs
+++ Assets/Script/MonoBehaviourEx.cs
@@ -4,14 +4,4 @@ using System.Collections;
 //! Base class for all mono behaviours.
 public abstract class MonoBehaviourEx : MonoBehaviour
 {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
