5f41ba36e3e2852f75f9ae89f75e0fe9fb8bb973
Trigve <trigves@yahoo.com>
2013-04-13 15:09:24 +0200

    Test: Add testing dir where testing script should be placed
    Each testing class should inherit from MonoBehavourTest

---
+++ Assets/Script/Test/MonoBehaviourTest.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Test
+{
+	//! Base class for all testing classes.
+	public abstract class MonoBehaviourTest : MonoBehaviourEx
+	{
+	}
+} /*Test*/
