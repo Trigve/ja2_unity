925b344452dd8c65d39de00abc3f176d5857b589
Trigve <trigves@yahoo.com>
2013-09-07 22:25:32 +0200

    ja2: Add TerrainTileHandle class

---
+++ game/ja2/TerrainTileHandle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ja2
+{
+	//! Handle for terrain tile.
+	[Serializable]
+	public struct TerrainTileHandle
+	{
+		//! X.
+		public int x;
+		//! Y.
+		public int y;
+
+		public TerrainTileHandle(int X, int Y)
+		{
+			x = X;
+			y = Y;
+		}
+	}
+}
