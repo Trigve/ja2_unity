96f222354f526c75f37f00dcc54e5c80ab0b1f3d
Trigve <trigves@yahoo.com>
2013-09-29 15:15:09 +0200

    editor: PrefabManagerEditor add solo method for creating from resources
    We need also methods to create prefabs from any asset folder, not
    only from Resources one.

--- Assets/Editor/PrefabManagerEditor.cs
+++ Assets/Editor/PrefabManagerEditor.cs
@@ -6,8 +6,18 @@ public static class PrefabManagerEditor
 	//! Instantiate prefab.
 	static public GameObject Create(string Name)
 	{
-		var prefab_class = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/" + Name + ".prefab", typeof(GameObject));
-		GameObject prefab = (GameObject)GameObject.Instantiate(prefab_class);
+		var prefab_class = AssetDatabase.LoadAssetAtPath("Assets/" + Name + ".prefab", typeof(GameObject));
+		GameObject prefab = (GameObject)PrefabUtility.InstantiatePrefab(prefab_class);
+		prefab.name = prefab_class.name;
+
+		return prefab;
+	}
+
+	//! Instantiate resource prefab.
+	static public GameObject CreateFromResources(string Name)
+	{
+		var prefab_class = Resources.Load("Prefabs/" + Name, typeof(GameObject));
+		GameObject prefab = (GameObject)PrefabUtility.InstantiatePrefab(prefab_class);
 		prefab.name = prefab_class.name;
 
 		return prefab;
