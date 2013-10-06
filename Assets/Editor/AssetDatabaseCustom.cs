7cea52252d05b62e46e29bfa28c30a7abe2072dd
Trigve <trigves@yahoo.com>
2013-10-06 16:53:41 +0200

    editor: Forget to add AssetDatabaseCustom class long ago

---
+++ Assets/Editor/AssetDatabaseCustom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+class AssetDatabaseCustom : ja2.script.IEditor
+{
+#region Interface
+	public string GetAssetPath(UnityEngine.Object AssetObject)
+	{
+		return AssetDatabase.GetAssetPath(AssetObject);
+	}
+
+	public void CreateAsset(UnityEngine.Object Asset, string Path)
+	{
+		AssetDatabase.CreateAsset(Asset, Path);
+	}
+
+	public UnityEngine.Object GetPrefabParent(UnityEngine.Object Instance)
+	{
+		return PrefabUtility.GetPrefabParent(Instance);
+	}
+	
+	//! PrefabUtility
+	public UnityEngine.Object InstantiatePrefab(UnityEngine.Object Target)
+	{
+		return PrefabUtility.InstantiatePrefab(Target);
+	}
+#endregion
+	
+}
